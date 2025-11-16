014824d1510d8b99d45d3f1bb4cfbc0b060f8756
zndk-hys <hysryt+zndk@gmail.com>
2025-11-16 21:14:43 +0900

    tag filtering

--- components/TagInput.tsx
+++ components/TagInput.tsx
@@ -6,20 +6,42 @@ import { MouseEventHandler, useState } from "react";
 export default function TagInput() {
   const [newTagName, setNewTagName] = useState('');
   const {isPendingLoad, isPendingAdd, tagList, addToTagList} = useTagList();
+  const [selectedTagIds, setSelectedTagIds] = useState<string[]>([]);
 
+  // タグ追加ボタン
   const handleAddTag: MouseEventHandler<HTMLButtonElement> = async (e) => {
     e.preventDefault();
     if(newTagName) {
-      await addToTagList(newTagName);
+      const newTag = await addToTagList(newTagName);
+      setSelectedTagIds(ids => [...ids, newTag.id]);
       setNewTagName('');
     }
   }
+
+  // タグ選択変更
+  const handleChangeSelect = (id: string, isChecked: boolean) => {
+    setSelectedTagIds(prevIds => {
+      return isChecked ? [...prevIds, id] : prevIds.filter(deleteId => deleteId !== id);
+    })
+  }
+
+  // タグのフィルタリング
+  const keyword = newTagName.toLocaleLowerCase().trim();
+  const filteredTagList =
+    keyword === ''
+      ? tagList.map(tag => tag.id)
+      : tagList.filter(tag => tag.name.toLowerCase().includes(keyword)).map(tag => tag.id);
 
   return (
     <>
       <div>
         タグの追加：<br />
-        <input type="text" value={newTagName} onChange={e => setNewTagName(e.target.value)} />
+        <input
+          type="text"
+          value={newTagName}
+          onChange={e => setNewTagName(e.target.value)}
+          onKeyDown={e => {if (e.key === 'Enter') { e.preventDefault()}}}
+        />
         <button onClick={handleAddTag} disabled={isPendingAdd}>追加</button>
         {isPendingAdd && '追加中'}
       </div>
@@ -27,8 +49,16 @@ export default function TagInput() {
         タグ：<br />
         <div style={{height: '200px', overflow: 'scroll'}}>
           {isPendingLoad ? <p>読み込み中</p> : tagList.map(tag => (
-            <div key={tag.id}>
-              <input type="checkbox" name="tags" value={tag.id} defaultChecked={tag.new} />{tag.name}
+            <div key={tag.id} style={{display: filteredTagList.includes(tag.id) ? 'block' : 'none'}}>
+              <label>
+                <input
+                  type="checkbox"
+                  name="tags"
+                  value={tag.id}
+                  onChange={e => handleChangeSelect(tag.id, e.target.checked)}
+                  checked={selectedTagIds.includes(tag.id)}
+                />{tag.name}
+              </label>
             </div>
           ))}
         </div>
--- hooks/useTagList.ts
+++ hooks/useTagList.ts
@@ -2,28 +2,31 @@ import addTag from "@/actions/addTag";
 import getTagList from "@/actions/getTagList";
 import { Tag } from "@/types/tag";
 import { MicroCMSContentId } from "microcms-js-sdk";
-import { useEffect, useState, useTransition } from "react";
+import { useEffect, useState } from "react";
 
 export default function useTagList() {
-  const [tagList, setTagList] = useState<(Tag & MicroCMSContentId & {new?: boolean})[]>([]);
-  const [isPendingLoad, startTransitionLoad] = useTransition();
+  const [tagList, setTagList] = useState<(Tag & MicroCMSContentId)[]>([]);
+  const [isPendingLoad, setIsPendingLoad] = useState(false);
   const [isPendingAdd, setIsPendingAdd] = useState(false);
 
   useEffect(() => {
-    startTransitionLoad(async () => {
-      const tagList = await getTagList();
-      setTagList(tagList.contents);
-    });
+    (async () => {
+      setIsPendingLoad(true);
+      getTagList().then(tagList => {
+        setTagList(tagList.contents);
+      }).finally(() => {
+        setIsPendingLoad(false);
+      })
+    })();
   }, [setTagList]);
 
   const addToTagList = async (newTagName: string) => {
     setIsPendingAdd(true);
     const res = await addTag(newTagName);
-    if (res.id) {
-      const newTag = { id: res.id, name: newTagName, new: true }
-      setTagList(tagList => [newTag, ...tagList]);
-    }
+    const newTag = { id: res.id, name: newTagName }
+    setTagList(tagList => [newTag, ...tagList]);
     setIsPendingAdd(false);
+    return newTag;
   }
 
   return {
