f7df8118e81e3fff2f032d1f6908e642e99b7428
zndk-hys <hysryt+zndk@gmail.com>
2025-11-16 18:16:44 +0900

    fix

--- actions/addBlog.ts
+++ actions/addBlog.ts
@@ -1,19 +1,28 @@
 'use server'
 
 import { createBlog } from "@/lib/microcms";
+import { BlogPost } from "@/types/blog";
 
 export default async function addBlog(formData: FormData) {
   const raw = {
     title: String(formData.get('title') ?? ''),
     body: String(formData.get('body') ?? ''),
     tags: formData.getAll('tags').map(tag => String(tag)) ?? [],
-    publishedAt: String(formData.get('publishedAt') ?? ''),
+    publishedAt: String(formData.get('publishedAt') ?? '').trim(),
     isDraft: String(formData.get('isDraft') ?? '') === 'on' ? true : false,
   }
 
-  // 下書きの場合はpublishedAtを無視
   const { isDraft, publishedAt, ...rest } = raw;
-  const content = isDraft ? rest : { ...rest, publishedAt };
+
+  // 公開かつ公開日が指定されている場合は追加
+  let content: BlogPost = rest;
+  if ( !isDraft && publishedAt ) {
+    content = {
+      ...content,
+      publishedAt,
+    }
+  };
 
   await createBlog(content, isDraft);
 }
+
