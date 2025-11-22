8dcbbd89cf417eca4b0dac3e20cd9434c2dc1737
zndk-hys <hysryt+zndk@gmail.com>
2025-11-23 05:11:17 +0900

    パスワードハッシュ化

--- actions/login.ts
+++ actions/login.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import argon2 from "argon2";
 import { SignJWT } from "jose";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
@@ -7,7 +8,8 @@ import { redirect } from "next/navigation";
 export default async function login(formData: FormData) {
   const alg = process.env.JWT_ALG;
   const base64Key = process.env.JWT_KEY;
-  if (!alg || !base64Key) {
+  const password = process.env.ADMIN_PASS;
+  if (!alg || !base64Key || !password) {
     throw new Error();
   }
 
@@ -16,31 +18,19 @@ export default async function login(formData: FormData) {
     password: String(formData.get('password') ?? ''),
   }
 
-  if (raw.user !== process.env.ADMIN_USER || raw.password !== process.env.ADMIN_PASS) {
-    return {
-      error: true,
-      message: 'ユーザー名またはパスワードが間違っています',
-    };
+  // ハッシュ化したパスワードが壊れているとエラーが出るためキャッチする
+  try {
+    const validPassword = await argon2.verify(password, raw.password);
+    if (raw.user !== process.env.ADMIN_USER || !validPassword) {
+      return invalidIdOrPassword();
+    }
+  } catch(e) {
+    return invalidIdOrPassword();
   }
 
-  /**
-   * JWT発行
-   */
+  // JWT発行
   const secretKey = Buffer.from(base64Key, 'base64');
-
-  const header = {
-    alg,
-  };
-
-  const payload = {
-    user: raw.user,
-  };
-
-  const jwt = await new SignJWT(payload)
-    .setProtectedHeader(header)
-    .setIssuedAt()
-    .setExpirationTime('1d')
-    .sign(secretKey);
+  const jwt = await issueJwt(alg, secretKey, raw.user);
 
   const cookieStore = await cookies();
   cookieStore.set('jwt', jwt, {
@@ -51,4 +41,35 @@ export default async function login(formData: FormData) {
   });
 
   redirect('/');
+}
+
+/**
+ * IDかパスワードが間違っていた時用のレスポンス
+ */
+function invalidIdOrPassword() {
+  return {
+    error: true,
+    message: 'ユーザー名またはパスワードが間違っています',
+  };
+}
+
+/**
+ * JWT発行
+ */
+async function issueJwt(alg: string, secretKey: Uint8Array<ArrayBufferLike>, user: string) {
+  const header = {
+    alg,
+  };
+
+  const payload = {
+    user,
+  };
+
+  const jwt = await new SignJWT(payload)
+    .setProtectedHeader(header)
+    .setIssuedAt()
+    .setExpirationTime('1d')
+    .sign(secretKey);
+  
+  return jwt;
 }
